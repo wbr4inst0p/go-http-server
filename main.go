a43c6f9ae65d1a8677fb38855ed0c74aaa5a8566
Luca Berno <lberno@wellcomm.it>
2023-10-20 14:35:17 +0200

    update golang server

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
+	"html"
+	"log"
 	"net/http"
-	"os"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
-}
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
