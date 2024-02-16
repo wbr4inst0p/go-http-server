3de3331c098c05b5105bf3a4475ae75bde00e6d9
lberno <lberno@wellcomm.it>
2024-02-16 11:57:15 +0100

    .

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi Back")
+		fmt.Fprintf(w, "Hi Back!!")
 	})
 
 	log.Fatal(http.ListenAndServe(":10000", nil))
